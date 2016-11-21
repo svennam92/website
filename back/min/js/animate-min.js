e50e48b7bf8ee28d104d0451a2a24ce9f40f667f
Sai Vennam <svennam@us.ibm.com>
2016-11-21 10:37:32 -0600

    add updated site

---
+++ back/min/js/animate-min.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){$(this).scrollTop()>1?($("header").addClass("sticky"),$(".btns").show("slow"),$("#facepic").css({width:"35px",height:"35px"}),$("#facepic").css("width","35px"),$(".filler").css("height","60px")):($("header").removeClass("sticky"),$("#facepic").css({width:"200px",height:"200px"}),$(".filler").css("height","220px"),$(".btns").hide("slow"))});
--- js/animate-min.js
+++ js/animate-min.js
@@ -1,1 +1,21 @@
-$(window).scroll(function(){$(this).scrollTop()>1?($("header").addClass("sticky"),$(".btns").show("slow"),$("#facepic").css({width:"35px",height:"35px"}),$("#facepic").css("width","35px"),$(".filler").css("height","60px")):($("header").removeClass("sticky"),$("#facepic").css({width:"200px",height:"200px"}),$(".filler").css("height","220px"),$(".btns").hide("slow"))});
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 1) {
+		$('header').addClass("sticky");
+		$(".btns").show();
+		$("#facepic").css({
+			'width': '35px',
+			'height': '35px'
+		});
+		$("#facepic").css('width', '35px');
+		$(".filler").css('height', '60px');
+	} else {
+		$('header').removeClass("sticky");
+		$("#facepic").css({
+			'width': '200px',
+			'height': '200px'
+		});
+		$(".filler").css('height', '220px');
+		if(screen.width > 1024)
+			$(".btns").hide();
+	}
+});
